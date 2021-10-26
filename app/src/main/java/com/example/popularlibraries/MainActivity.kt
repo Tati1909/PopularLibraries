6efb5297295047f3478fcb6519b24f15475111ea
татьяна <emuart155@gmail.com>
2021-10-26 12:15:38 +0300

    ДЗ c ошибками

--- app/src/main/java/com/example/popularlibraries/MainActivity.kt
+++ app/src/main/java/com/example/popularlibraries/MainActivity.kt
@@ -1,59 +1,38 @@
 package com.example.popularlibraries
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.popularlibraries.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainView {
 
-    private var binding: ActivityMainBinding? = null
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
 
-    val counters = mutableListOf (0, 0, 0)
+    val presenter = Presenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
-        binding?.btnCounter1?.setOnClickListener {
-            binding?.btnCounter1?.text = (++counters[0]).toString()
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val listener = View.OnClickListener {
+            presenter.counterClick(it.id)
         }
-        binding?.btnCounter2?.setOnClickListener {
-            binding?.btnCounter2?.text = (++counters[1]).toString()
-        }
-        binding?.btnCounter3?.setOnClickListener {
-            binding?.btnCounter3?.text = (++counters[2]).toString()
-        }
-        initViews()
+        binding.btnCounter1.setOnClickListener(listener)
+        binding.btnCounter2.setOnClickListener(listener)
+        binding.btnCounter3.setOnClickListener(listener)
     }
 
-    fun initViews() {
-        binding?.btnCounter1?.text = counters[0].toString()
-        binding?.btnCounter2?.text = counters[1].toString()
-        binding?.btnCounter3?.text = counters[2].toString()
-    }
+    //Подсказка к ПЗ: поделить на 3 отдельные функции и избавиться от index
+    override fun setButtonText(index: Int, text: String) {
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putIntArray("counters", counters.toIntArray())
-    }
-
-    override fun onSaveInstanceState(
-        outState: Bundle,
-        outPersistentState: PersistableBundle
-    ) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        outState.putIntArray("counters", counters.toIntArray())
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        val countersArray = savedInstanceState.getIntArray("counters")
-        countersArray?.toList()?.let {
-            counters.clear()
-            counters.addAll(it)
+        when (index) {
+            0 -> _binding?.btnCounter1?.text = text
+            1 -> _binding?.btnCounter2?.text = text
+            2 -> _binding?.btnCounter3?.text = text
         }
-        initViews()
     }
 }
--- app/src/main/java/com/example/popularlibraries/Model.kt
+++ app/src/main/java/com/example/popularlibraries/Model.kt
@@ -1,4 +1,24 @@
 package com.example.popularlibraries
 
 class Model {
+
+    val counters = mutableListOf (0, 0, 0)
+
+    //вернуть текущий элемент списка
+    fun getCurrent(index: Int): Int {
+
+        return counters[index]
+    }
+
+    //увеличиваем выбранный счетчик и возвращаем его
+    fun next(index: Int): Int {
+
+        counters[index]++
+        return getCurrent(index)
+    }
+
+    fun set(index: Int, value: Int){
+
+        counters[index] = value
+    }
 }
